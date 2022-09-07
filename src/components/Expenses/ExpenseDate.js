a4d259c6686086207ecfc31db65c6007af76f48f
JacksonJW <jacksonjwatkins@gmail.com>
2022-09-07 05:57:35 -0700

    Correct classname to className

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -8,7 +8,7 @@ const ExpenseDate = (props) => {
   const year = props.date.getFullYear();
 
   return (
-    <div classname="expense-date">
+    <div className="expense-date">
       <div className="expense-date__month">{month}</div>
       <div className="expense-date__year">{year}</div>
       <div className="expense-date__day">{day}</div>
